ce88164612ad14a9bf4ae5def75d1a5630003508
blair <blair@10.0.0.182>
2018-06-02 22:49:29 -0500

    Added a 3rd functional test and cleaned up code/clarified Wait statements.

--- 5312018/src/WebformTest/ChromeDriverTest.java
+++ 5312018/src/WebformTest/ChromeDriverTest.java
@@ -3,7 +3,9 @@ package WebformTest;
 import java.util.concurrent.TimeUnit;
 
 
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -53,7 +55,7 @@ public class ChromeDriverTest {
         // 5. Fill out Phone Number
         WebElement PhoneNum = driver.findElement(By.xpath(".//*[@name='phone']"));
 
-       // Type a random Phone Number to the form. This needs not be unique.-
+       // Type a random Phone Number to the form. This needs not be unique.
         PhoneNum.sendKeys("999-999-9999");
         System.out.println("Phone Number has been filled out");
 
@@ -72,6 +74,9 @@ public class ChromeDriverTest {
         builder.moveToElement(HoverSubmit).build().perform();
 		String Color = HoverSubmit.getCssValue("background-color");
         System.out.println("After hovering, the submit button color is: " + Color);
+        if(PreColor != Color) {
+        	System.out.println(("Colors do not match, therefore the submit button changed color successfully on hover event."));
+        }
         
         // Submit the sign up form
         WebElement ClickSubmit = driver.findElement(By.xpath("//input[@type='submit']"));
@@ -79,41 +84,92 @@ public class ChromeDriverTest {
         String title = "Pilot Form Confirmation";
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.titleContains(title));
-        System.out.println("Functional Test 1 successful and form filled out correctly");
+        System.out.println("Functional Test 1 successful, form filled out correctly and user was directed to Thank You Page");
         driver.close();
         
 //--------------------------------------------FUNCTIONAL TEST 2--------------------------------------------//
-        //Test Submitting an empty form
+        //Test submitting incorrect characters and not directing user to Thank You page.
+        driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(baseURL);
+
+		// Fill out all fields        
+		// 1. Fill out Contact Name       
+        WebElement ContactName2 = driver.findElement(By.xpath(".//*[@name='firstname']"));  
+		ContactName2.sendKeys("Blaire");
+        System.out.println("ContactName has been filled out");
+        
+        // 2. Fill out Job Title
+        WebElement JobTitle2 = driver.findElement(By.xpath(".//*[@name='jobtitle']"));
+        JobTitle2.sendKeys("Tester");
+        System.out.println("JobTitle has been filled out");
+        
+       // 3. Fill out CompanyName
+        WebElement XOi2 = driver.findElement(By.xpath(".//*[@name='company']"));
+        XOi2.sendKeys("XOI is awesome");
+        System.out.println("Company Name has been filled out");
+        
+       // 4. Fill out CompanyEmail
+        WebElement CompanyEmail2 = driver.findElement(By.xpath(".//*[@name='email']"));
+        CompanyEmail2.sendKeys("asdfasdf");
+        System.out.println("Company Email has been filled out");
+        
+        // 5. Fill out Phone Number in nonconventional char form
+        WebElement PhoneNum2 = driver.findElement(By.xpath(".//*[@name='phone']"));
+        PhoneNum2.sendKeys("asdfas");
+        System.out.println("Phone Number has been filled out");
+        
+        WebElement ClickSubmit2 = driver.findElement(By.xpath("//input[@type='submit']"));       
+        ClickSubmit2.click();
+        String baseURLtobe = "http://info.xoi.io/pilot";
+        WebDriverWait waitattest2 = new WebDriverWait(driver, 10);
+        waitattest2.until(ExpectedConditions.urlToBe(baseURLtobe));
+        System.out.println("Functional Test 2 successful due to incorrect character input and did not direct user to Thank You page.");
+        driver.close();
+
+        		
+        
+        
+//------------------------------------------FUNCTIONAL TEST 3--------------------------------------------//
+        
+        //Test Submitting an empty form and not directing user to Thank You page
         
         driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(baseURL);
 		
-        // 1. Fill out Contact Name        
-        WebElement ContactName2 = driver.findElement(By.xpath(".//*[@name='firstname']"));  
-        ContactName2.click();
+        // 1. Fill out Contact Name    
+        WebElement ContactName3 = driver.findElement(By.xpath(".//*[@name='firstname']"));  
+        ContactName3.click();
+        ContactName3.sendKeys(Keys.SPACE);
         
         // 2. Fill out Job Title
-        WebElement JobTitle2 = driver.findElement(By.xpath(".//*[@name='jobtitle']"));
-        JobTitle2.click();
+        WebElement JobTitle3 = driver.findElement(By.xpath(".//*[@name='jobtitle']"));
+        JobTitle3.click();
+        JobTitle3.sendKeys(Keys.SPACE);
         
        // 3. Fill out CompanyName
-        WebElement XOi2 = driver.findElement(By.xpath(".//*[@name='company']"));
-        XOi2.click();
+        WebElement XOi3 = driver.findElement(By.xpath(".//*[@name='company']"));
+        XOi3.click();
+        XOi3.sendKeys(Keys.SPACE);
         
        // 4. Fill out CompanyEmail
-        WebElement CompanyEmail2 = driver.findElement(By.xpath(".//*[@name='email']"));
-        CompanyEmail2.click();
+        WebElement CompanyEmail3 = driver.findElement(By.xpath(".//*[@name='email']"));
+        CompanyEmail3.click();
+        CompanyEmail3.sendKeys(Keys.SPACE);
         
         // 5. Fill out Phone Number
-        WebElement PhoneNum2 = driver.findElement(By.xpath(".//*[@name='phone']"));
-        PhoneNum2.click();
+        WebElement PhoneNum3 = driver.findElement(By.xpath(".//*[@name='phone']"));
+        PhoneNum3.click();
+        PhoneNum3.sendKeys(Keys.SPACE);
         
         // Submit the sign up form
-        WebElement ClickSubmit2 = driver.findElement(By.xpath("//input[@type='submit']"));
-        ClickSubmit2.click();
-        WebDriverWait waitagain = new WebDriverWait(driver, 10);
-        waitagain.until(ExpectedConditions.titleContains(title));    
+        WebElement ClickSubmit3 = driver.findElement(By.xpath("//input[@type='submit']"));       
+        ClickSubmit3.click();
+        WebDriverWait wait3 = new WebDriverWait(driver, 10);
+        wait3.until(ExpectedConditions.urlToBe(baseURLtobe));
+        System.out.println("Functional Test 3 successful due to nonentries in form and did not direct user to Thank You page.");
+        driver.close();
         
 		
 	}
